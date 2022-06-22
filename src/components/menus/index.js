927ab82210e72b306f7e3102e8217b53005766e7
bvjingee <bvjingeemarchii@gmail.com>
2022-06-22 11:12:03 +0800

    Uurchilsun

--- src/components/menus/index.js
+++ src/components/menus/index.js
@@ -1,8 +1,8 @@
 import React from "react";
+import "./style.css";
 
 function Menubar() {
-  return;
-  <div className="container">Menu</div>;
+  return <div className="container">Menu</div>;
 }
 
 export default Menubar;
