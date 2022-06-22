54e15673f84201d22af8402f974aa9a3d66b1f4d
bvjingee <bvjingeemarchii@gmail.com>
2022-06-22 12:58:01 +0800

    Specials heseg deer nemsen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Menubar from "./components/menus";
+import Specials from "./components/Our specials/specials";
 
 function App() {
   return (
     <div className="App">
       <div className="app">
         <Menubar />
+        <Specials />
       </div>
     </div>
   );
--- src/components/Our specials/specials.js
+++ src/components/Our specials/specials.js
@@ -1,7 +1,34 @@
 import React from "react";
+import "./special.css";
 
 function Specials() {
-  return <div>Our Specials</div>;
+  return (
+    <div className="container">
+      <h1>Our Specials</h1>
+      <div className="arrow"></div>
+      <div className="boxs">
+        <div className="box">
+          <h2>Lorem imsum dolor</h2>
+          <img src="https://images.pexels.com/photos/299348/pexels-photo-299348.jpeg?cs=srgb&dl=pexels-terje-sollie-299348.jpg&fm=jpg"></img>
+          <p>Lorem ipsum dolor set amet</p>
+        </div>
+      </div>
+      <div className="boxs">
+        <div className="box">
+          <h2>Lorem imsum dolor</h2>
+          <img src="https://images.pexels.com/photos/299348/pexels-photo-299348.jpeg?cs=srgb&dl=pexels-terje-sollie-299348.jpg&fm=jpg"></img>
+          <p>Lorem ipsum dolor set amet</p>
+        </div>
+      </div>
+      {/* <div className="boxs">
+        <div className="box">
+          <h2>Lorem imsum dolor</h2>
+          <img src="https://images.pexels.com/photos/299348/pexels-photo-299348.jpeg?cs=srgb&dl=pexels-terje-sollie-299348.jpg&fm=jpg"></img>
+          <p>Lorem ipsum dolor set amet</p>
+        </div>
+      </div> */}
+    </div>
+  );
 }
 
 export default Specials;
